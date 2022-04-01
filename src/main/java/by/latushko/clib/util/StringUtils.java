9d54bdde7672e69c82860be51722f7d30fc90fc0
Pavel <plat956@gmail.com>
2022-04-01 14:39:08 +0300

    fix

--- src/main/java/by/latushko/clib/util/StringUtils.java
+++ src/main/java/by/latushko/clib/util/StringUtils.java
@@ -1,8 +1,6 @@
 package by.latushko.clib.util;
 
 public class StringUtils {
-    private static final String NEGATIVE_SIGN = "-";
-
     public boolean isPositiveNumber(String str) {
         return org.apache.commons.lang3.StringUtils.isNumeric(str);
     }
